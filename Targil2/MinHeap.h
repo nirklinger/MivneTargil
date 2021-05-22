d30a7704aeaeefc8862bc3de205ab5c07ead6ff4
Nir Klinger <nirkl@mta.ac.il>
2021-05-22 12:42:21 +0300

    minHeapClass

--- Targil2/MinHeap.h
+++ Targil2/MinHeap.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef _MIN_HEAP_H_
+#define _MIN_HEAP_H_
+template<class T>
+class MinHeap {
+	T* heapArr; // pointer to array of elements in heap
+	int capacity; // maximum possible size of min heap
+	int size; // Current number of elements in min heap
+	int left(const int index) const;
+	int right(const int index) const;
+	int parent(const int index) const;
+	void heapifyUp(const int index);
+	bool isLeaf(const int index) const;
+public: 
+	void heapifyDown(const int index);
+	void insert(T element);
+	T& deleteMin();
+	const T& getMin();
+	MinHeap(int capacity);
+};
+
+#endif
